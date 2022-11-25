420f99aee86ed4f9f27f7781ff4286ad073eab87
nick <nckdlby@gmail.com>
2022-11-25 17:25:23 +0100

    script

--- main.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector('.menu')
-const mobMenu = document.querySelector('.mobile-menu')
-
-menuBtn.addEventListener('click', () => {
-  mobMenu.classList.toggle('open')
-})
-
